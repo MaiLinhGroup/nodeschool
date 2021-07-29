9a39c5c9e7f1f702b0a7bca693c68a4b432f335e
ml <ml@mac.os>
2021-07-29 21:48:04 +0200

    learn how to filter in folder

---
+++ learnyounode/filtered-ls.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const path = require('path')
+
+const dirPath = process.argv[2]
+const fileExtension = process.argv[3]
+
+fs.readdir(dirPath, (err, fileNames) => {
+    if (err) return console.error(err)
+    fileNames.forEach(fileName => {
+        if(path.extname(fileName).endsWith(fileExtension)) {
+            console.log(fileName)
+        }
+    })
+})
