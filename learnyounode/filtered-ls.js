1957bafbc94d943eddcda266b413d659d6ec1bd9
ml <ml@mac.os>
2021-07-29 22:38:17 +0200

    improve filter for extension solution

--- learnyounode/filtered-ls.js
+++ learnyounode/filtered-ls.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const path = require('path')
 
 const dirPath = process.argv[2]
-const fileExtension = process.argv[3]
+const fileExtension = "." + process.argv[3]
 
 fs.readdir(dirPath, (err, fileNames) => {
     if (err) return console.error(err)
     fileNames.forEach(fileName => {
-        if(path.extname(fileName).endsWith(fileExtension)) {
+        if (path.extname(fileName) === fileExtension){
             console.log(fileName)
         }
-    })
+    }) 
 })
