30774267a499b8cfe2747bd2c562947e0be4ec11
ml <ml@mac.os>
2021-07-29 21:00:26 +0200

    mv files

--- my-first-io.js
+++
@@ -1,6 +0,0 @@
-// load global module into const
-const fs = require('fs')
-// read in file content as string and split it by newlines to count number of newlines
-const numNewLines = fs.readFileSync(process.argv[2],'utf8').split('\n').length-1
-
-console.log(numNewLines)
