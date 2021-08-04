6a0f12ac949924644d49c7a94b9f735011199dee
ml <ml@mac.os>
2021-08-04 20:21:53 +0200

    juggling async without async

--- learnyounode/juggling-async.js
+++ learnyounode/juggling-async.js
@@ -1,1 +1,29 @@
 const http = require('http')
+const args = process.argv.slice(2)
+let contents = []
+let count = 0 
+
+function collectContent(url, index) {
+    http.get(url, (response) => {
+        let content = ''
+
+        response.setEncoding('utf8')
+        response.on('data', (chunk) => {
+            content += chunk
+        })
+        response.on('end', () => {
+            contents[index] = content
+            count++
+
+            if(count == 3) printContents()
+        })
+    })
+}
+
+function printContents() {
+    contents.forEach(content => console.log(content))
+}
+
+for(let i = 0; i<3; i++) {
+    collectContent(args[i], i)
+}
